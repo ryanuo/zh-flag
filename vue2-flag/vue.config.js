645b0a1a62d8a5d3259ac92c512058720749b66a
rr210 <1577791638@qq.com>
2021-10-01 20:18:34 +0800

    替换相对路径

---
+++ vue2-flag/vue.config.js
@@ -0,0 +1,15 @@
+/***
+ * @Description: 
+ * @Author: Harry
+ * @Date: 2021-10-01 20:16:39
+ * @Url: https://u.mr90.top
+ * @github: https://github.com/rr210
+ * @LastEditTime: 2021-10-01 20:16:39
+ * @LastEditors: Harry
+ */
+// vue.config.js
+module.exports = {
+  // ...
+  publicPath: './'
+  // ...
+};
